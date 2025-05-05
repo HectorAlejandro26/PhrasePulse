c39cbc6b191c36f008f6ea8cbadad32621657388
HectorAlejandro26 <alex2611alejandro@gmail.com>
2025-05-04 21:19:23 -0700

    Actualiza opciones de pulsos y mejora validaciones
    Se ha añadido la directiva `using System.Text.RegularExpressions;` para el uso de expresiones regulares. Los valores de la enumeración `PulseOptions` se han modificado a una representación binaria de 8 bits.
    
    Se ha eliminado la propiedad `FullWords` de `PulseOptionsManager`, y se ha añadido una validación para el parámetro `matchTimeout`, asegurando que no sea negativo. La lógica del constructor se ha ajustado para establecer un valor predeterminado de 3 segundos.
    
    Además, se ha eliminado la opción "CompleteWords" de la representación de cadena de las opciones.

--- Logic/Configuration/PulseOptionsManager.cs
+++ Logic/Configuration/PulseOptionsManager.cs
@@ -1,15 +1,16 @@
 ﻿using System.Diagnostics;
+using System.Text.RegularExpressions;
 
 namespace PhrasePulse.Logic.Configuration;
 
 public enum PulseOptions
 {
-    Default = 0b0000,
-    CaseInsensitive = 0b0001,
-    CompleteWords = 0b0010,
-    UseRegex = 0b0100,
-    NoColor = 0b1000,
-    AllowOverlapping = 0b10000
+    Default = 0b00000000,
+    CaseInsensitive = 0b00000001,
+    UseRegex = 0b00000010,
+    NoColor = 0b00000100,
+    AllowOverlapping = 0b00001000,
+    ShowPhraseColored = 0b00010000
 }
 
 [DebuggerDisplay($"{{{nameof(GetDebuggerDisplay)}(),nq}}")]
@@ -17,7 +18,6 @@ internal readonly struct PulseOptionsManager
 {
     public readonly bool UsingDefault;
     public readonly bool IgnoreCase;
-    public readonly bool FullWords;
     public readonly bool UseRegex;
     public readonly bool NoColor;
     public readonly bool AllowOverlapping;
@@ -32,19 +32,20 @@ internal readonly struct PulseOptionsManager
         UsingDefault = options == PulseOptions.Default;
 
         IgnoreCase = (options & PulseOptions.CaseInsensitive) == PulseOptions.CaseInsensitive;
-        FullWords = (options & PulseOptions.CompleteWords) == PulseOptions.CompleteWords;
         UseRegex = (options & PulseOptions.UseRegex) == PulseOptions.UseRegex;
         NoColor = (options & PulseOptions.NoColor) == PulseOptions.NoColor;
         AllowOverlapping = (options & PulseOptions.AllowOverlapping) == PulseOptions.AllowOverlapping;
-        MatchTimeout = matchTimeout ?? 3;
 
-        if (UseRegex && FullWords)
+        if (matchTimeout < 0)
         {
             throw new PhrasePulseException(
-                "Please use only one of these options at a time.",
-                new ArgumentException("UseRegex cannot be used with CaseInsensitive options.")
+                "If null, the default value of 3 seconds will be used.",
+                new RegexMatchTimeoutException(
+                    "Match timeout cannot be negative. Use null to set the default value."
+                )
             );
         }
+        MatchTimeout = matchTimeout ?? 3;
     }
 
     public static PulseOptionsManager Default => new(PulseOptions.Default);
@@ -55,7 +56,6 @@ internal readonly struct PulseOptionsManager
 
         List<string> options = [];
         if (IgnoreCase) options.Add("CaseInsensitive");
-        if (FullWords) options.Add("CompleteWords");
         if (UseRegex) options.Add("UseRegex");
         if (NoColor) options.Add("NoColor");
         if (AllowOverlapping) options.Add("AllowOverlapping");
